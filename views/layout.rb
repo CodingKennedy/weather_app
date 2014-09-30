3e03e49a5251f39000ec2d81a84cc71467e9b848
SpiritJinja66 <spiritninja66@aol.com>
2014-09-30 13:32:51 -0400

    Weather conditions now work

--- views/layout.rb
+++ views/layout.rb
@@ -2,9 +2,10 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <link rel="stylesheet" href="index.css" type="text/css" />
   <title>weather app</title>
 </head>
-<body>
+<body style = "background: url('images/map.png');">
 <%= yield%>
 </body>
 </html>
