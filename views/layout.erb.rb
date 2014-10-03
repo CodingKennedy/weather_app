1266a0303ec006cdcc7cd6983191123b5c069d43
SpiritJinja66 <spiritninja66@aol.com>
2014-10-03 16:47:03 -0400

    backgrounds still not working.

---
+++ views/layout.erb.rb
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link href="/public/style.css" rel="stylesheet">
+  <title>weather app</title>
+</head>
+<body>
+<%= yield %>
+</body>
+</html>
+
